9f782373744300849fd859dd9d1bda3e3751bcf6
DagrMine <32650785+DagrMine@users.noreply.github.com>
2018-05-17 21:18:11 -0400

    Create Dragonmark/created .cs

--- Items/Weapons/Dragonblade.cs
+++ Items/Weapons/Dragonblade.cs
@@ -8,20 +8,20 @@ namespace Megaloguns.Items
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Dragonblade");
-			Tooltip.SetDefault("This is a modded sword.");
+			Tooltip.SetDefault("Ryūjin no ken wo kurae!);
 		}
 		public override void SetDefaults()
 		{
-			item.damage = 50;
+			item.damage = 80;
 			item.melee = true;
-			item.width = 40;
-			item.height = 40;
-			item.useTime = 20;
-			item.useAnimation = 20;
-			item.useStyle = 1;
+			item.width = 100;
+			item.height = 100;
+			item.useTime = 15;
+			item.useAnimation = 15;
+			item.useStyle = 2;
 			item.knockBack = 6;
-			item.value = 10000;
-			item.rare = 2;
+			item.value = 1000;
+			item.rare = -1;
 			item.UseSound = SoundID.Item1;
 			item.autoReuse = true;
 		}
@@ -29,7 +29,9 @@ namespace Megaloguns.Items
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.AddIngredient(ItemID.SoulofMight, 5);
+			recipe.AddIngredient(ItemID.HallowedBar, 10);
+			recipe.AddIngredient(ItemID.DragonMark, 1);
 			recipe.AddTile(TileID.WorkBenches);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
